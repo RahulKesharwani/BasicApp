75bc33f95b2689e6840514d07fb6cdfd527f7dfa
Rahul Kesharwani <rahul.kesharwani.m2nr@gmail.com>
2018-08-15 16:27:04 +0530

    Adding final touch to basic form

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,30 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl,FormArray, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import * as cloneDeep from 'lodash/cloneDeep';
-import { UserRequest } from '../../../models/user-request';
 
 @Component({
-  selector: 'app-user-form',
-  templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
 })
-export class UserFormComponent implements OnInit {
+export class AppComponent  {
+  name = 'Angular';
   userForm: FormGroup;
-  
   jobTypes = [
-    { id: 1, name: 'Onsite' },
-    { id: 2, name: 'Partial Remote' },
-    { id: 3, name: 'Full Time Remote' }
+    { id: 1, name: 'Onsite', value: "OS" },
+    { id: 2, name: 'Partial Remote', value: "PR" },
+    { id: 3, name: 'Full Time Remote', value: "FR" }
   ];
 
-  technologies = ['Front-end development', 'Back-end development', 'Full-stack developement','Data Science', 'DB Administrator'];
-  constructor(private formBuilder: FormBuilder) { 
+  technologies = ['Front-end development', 'Back-end development', 'Full-stack developement','Data Science', 'DB Administrator']; 
+constructor(private formBuilder: FormBuilder) { 
     const controls = this.jobTypes.map(c => new FormControl(false));
     controls[0].setValue(true); // Set the first checkbox to 
     this.userForm = this.createFormGroupWithBuilder(formBuilder , controls);
-  }
-
-  ngOnInit() {
   }
 
   createFormGroupWithBuilder(formBuilder: FormBuilder, controls) {
@@ -43,18 +40,35 @@ export class UserFormComponent implements OnInit {
 
 
   onSubmit(){
-    const result: UserRequest = cloneDeep(Object.assign({}, this.userForm));
+    const result: any = Object.assign({}, this.userForm);
     console.log('after submit');
     console.log(result);
-    const selectedOrderIds = this.userForm.value.jobTypes
-      .map((v, i) => v ? this.jobTypes[i].id : null)
-      .filter(v => v !== null);
-      console.log(selectedOrderIds);
+    result.value.jobTypes = this.findJobType(this.userForm.value.jobTypes);
+    console.log(JSON.stringify(result.value));
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(result.value));
   }
 
   revert(){
     this.userForm.reset(UserRequest);
   }
 
- 
+   findJobType(selectedJobs : string[]){
+     let jobTypes = [];
+    for(let job in selectedJobs){
+      jobTypes.push(this.jobTypes[job]['value']);
+    }
+    return jobTypes; 
+   }
+}
+
+
+export class UserRequest {
+    firstName: string = '';
+    lastName: string = '';
+    city: string = '';
+    gender: string = '';
+    tech: string = '';
+    email: string = '';
+    phoneNumber: string = '';
+    message: string = '';
 }
